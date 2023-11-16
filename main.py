a805d7cf02b17a57502926f2d572f690a1aa1f0f
Arthur C <arthur_07q@hotmail.com>
2023-11-16 14:50:51 -0300

    Barebones for multiple models

--- main.py
+++ main.py
@@ -17,21 +17,22 @@ from utils.fine_tuning import FineTuning
 from utils.dataset_setup import dataset_setup
 
 
+models = {
+    'AlexNet': AlexNet,
+    'DenseNet': DenseNet,
+    'GoogLeNet': GoogLeNet,
+    'MNASNet': MNASNet,
+    'MobileNetV2': MobileNetV2,
+    'ResNet34': ResNet34,
+    'ResNeXt': ResNeXt,
+    'ShuffleNetV2': ShuffleNetV2,
+    'SqueezeNet': SqueezeNet,
+    'VGG': VGG,
+    'WideResNet': WideResNet
+}
+
 
 def train_model(model_name: str, epochs: int, lr: float, weight_decay: float) -> None:
-    models = {
-        'AlexNet': AlexNet,
-        'DenseNet': DenseNet,
-        'GoogLeNet': GoogLeNet,
-        'MNASNet': MNASNet,
-        'MobileNetV2': MobileNetV2,
-        'ResNet34': ResNet34,
-        'ResNeXt': ResNeXt,
-        'ShuffleNetV2': ShuffleNetV2,
-        'SqueezeNet': SqueezeNet,
-        'VGG': VGG,
-        'WideResNet': WideResNet
-    }
     model = models[model_name](lr, weight_decay)
     net = model.net
     optimizer = model.optimizer
@@ -40,13 +41,17 @@ def train_model(model_name: str, epochs: int, lr: float, weight_decay: float) ->
     preds = fine_tuning.get_predictions()
     print('Computing classification report')
     report = classification_report(target, preds, output_dict=True)
-    print(report)
     return report
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='ResNet34')
+    parser.add_argument(
+        '--models',
+        type=str, 
+        default='ResNet34', 
+        choices=models.keys(),
+        nargs='*')
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--lr', type=float, default=1e-5)
     parser.add_argument('--weight_decay', type=float, default=5e-4)
@@ -55,11 +60,14 @@ def get_args():
 
 def main(args) -> None:
     dataset_setup()
-    train_model(
-        model_name=args.model,
-        epochs=args.epochs,
-        lr=args.lr,
-        weight_decay=args.weight_decay)
+    for model in args.models:
+        report = train_model(
+            model_name=model,
+            epochs=args.epochs,
+            lr=args.lr,
+            weight_decay=args.weight_decay)
+        print(report.keys())
+        print(report)
 
 
 if __name__ == '__main__':
